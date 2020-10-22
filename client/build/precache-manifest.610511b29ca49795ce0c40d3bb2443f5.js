79fb27baa8a881d86a6feeab88f84410a20fbee0
Anjini Krishnan <anjini.krishnan@hotmail.com>
2020-10-22 21:10:07 +1030

    fixing heroku too

---
+++ client/build/precache-manifest.610511b29ca49795ce0c40d3bb2443f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ba213a97765211fc0f5bf4553c1f7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8b227aa163a5d434b73",
+    "url": "/static/css/main.88c04b05.chunk.css"
+  },
+  {
+    "revision": "f0d2c782e07a6efe3837",
+    "url": "/static/js/2.fb79f6a6.chunk.js"
+  },
+  {
+    "revision": "247a729dcee921389bb46e1b033608e4",
+    "url": "/static/js/2.fb79f6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b227aa163a5d434b73",
+    "url": "/static/js/main.a571f6b0.chunk.js"
+  },
+  {
+    "revision": "2fff04eb8d07d01e602d",
+    "url": "/static/js/runtime-main.e52e0b8f.js"
+  },
+  {
+    "revision": "ce133ebd7477c505ff69847f90878118",
+    "url": "/static/media/b&w.ce133ebd.jpg"
+  },
+  {
+    "revision": "0c981314febfa968bab9990afc67a15f",
+    "url": "/static/media/foodAndMenus.0c981314.jpg"
+  },
+  {
+    "revision": "8d871736460c3723926de6cb04301faf",
+    "url": "/static/media/order.8d871736.jpg"
+  }
+]);
--- client/src/components/SignInForm/index.js
+++ client/src/components/SignInForm/index.js
@@ -1,31 +1,30 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import API from "../../utils/API";
 
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
 function SignInForm() {
-// Setting our component's initial state
-const [email, setEmail] = useState();
-const [password, setPassword] = useState();
+  // Setting our component's initial state
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-  if (email && password) {
-    API.loginUser({
-      email: email,
-      password: password
-    }).then(function(data) {
-      console.log("Login Response: " + data);
-        window.location.replace("/home/"+data.data._id);
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (email && password) {
+      API.loginUser({
+        email: email,
+        password: password
+      }).then(function (data) {
+        window.location.replace("/home/" + data.data._id);
       })
-      .catch(err => console.log(err));
+        .catch(err => console.log(err));
+    }
   }
-}
   return (
     <form onSubmit={handleFormSubmit}>
       <div className="form-group">
         <label htmlFor="email">Email address:</label>
         <input
-          onChange={e=>setEmail(e.target.value)}
+          onChange={e => setEmail(e.target.value)}
           name="email"
           type="text"
           className="form-control"
@@ -34,7 +33,7 @@ function handleFormSubmit(event) {
         />
         <label htmlFor="password">password:</label>
         <textarea
-          onChange={e=>setPassword(e.target.value)}
+          onChange={e => setPassword(e.target.value)}
           name="password"
           type="password"
           className="form-control"
